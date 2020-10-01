116f38a9129a004e24bf8c887e7d144dc5f277b2
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-10-01 17:13:21 -0400

    Create google_places_details.py

---
+++ google_places_details.py
@@ -0,0 +1,11 @@
+gkey = "your google api key"
+# Dependencies
+
+import requests
+import json
+import pandas as pd
+from pandas import json_normalize 
+import time
+import os
+from os import listdir
+from os.path import isfile, join
